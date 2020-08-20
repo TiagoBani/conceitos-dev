ac73b63ed512e2e58395323c7c76ee8c12d24982
tiagobani <tiago.bani@yahoo.com.br>
2020-08-20 06:39:31 -0300

    [feat]: add mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,80 @@
+import React, {useEffect, useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import api from './services/api';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+  project: {
+    color: '#FFF',
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+const App = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      tittle: `Novo projeto ${Date.now()}`,
+      owner: 'Tiago',
+    });
+
+    setProjects([...projects, response.data]);
+  }
+
+  return (
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#7159c1"
+        translucent={true}
+      />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({item}) => (
+            <Text style={styles.project}>{item.tittle}</Text>
+          )}
+        />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}>
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
